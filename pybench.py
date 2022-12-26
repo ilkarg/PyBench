6ce0009df34f2bf603ff4fa83c45ed893a6358f6
Ilya <60040129+mrProger@users.noreply.github.com>
2022-12-26 11:51:37 +0600

    Добавлен аргумент в декоратор для выбора выводить результат в консоль или записывать в файл

--- pybench.py
+++ pybench.py
@@ -1,9 +1,27 @@
 from datetime import datetime
+import os.path
 
-def benchmark(input_func):
-	def output_func(*args):
-		start_time = datetime.now()
-		ret = input_func(*args)
-		print(f'{input_func.__name__}: {datetime.now() - start_time}')
-		return ret
-	return output_func
+def benchmark(logging):
+    if not type(logging) == bool:
+        raise TypeError('Аргумент должен иметь тип boolean')
+    def benchmark_without_logs(input_func):
+        def func(*args):
+            start_time = datetime.now()
+            ret = input_func(*args)
+            print(f'{input_func.__name__}: {datetime.now() - start_time}')
+            return ret
+        return func
+    def benchmark_with_logs(input_func):
+        def func(*args):
+            start_time = datetime.now()
+            ret = input_func(*args)
+            if not os.path.exists('PyBench'):
+                os.mkdir('PyBench')
+            with open('PyBench/benchmark.pybench', 'a') as file:
+                file.write(f'{input_func.__name__}: {datetime.now() - start_time}\n')
+            return ret
+        return func
+    if not logging:
+        return benchmark_without_logs
+    else:
+        return benchmark_with_logs
